d1f1e292f3c53bd2b04e41b8f8799a913c629092
roshankrsu <roshankrsu@gmail.com>
2025-07-07 19:02:55 +0000

    data types

---
+++ basics/dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"// treat all js code as newer version
+
+// alert( 5 + 2 )//only works in browser
+
+console.log(3+4)//code readablilit must be high
+
+console.log("ros")
+
+
+let name = "roshan"
+let age = 20
+let isLoggedIn = false
+
+//nmber=> 2 to power 53 
+//bigint
+//string =>""
+//boolean => true/false
+//null => standalone value
+// undefined => 
+// symbol => unique
+
+// object 
+
+console.log(typeof null);
