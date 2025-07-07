3536121452db8c6a8f657b543991e3d865d8e343
roshankrsu <roshankrsu@gmail.com>
2025-07-07 18:31:43 +0000

    variables

---
+++ basics/variables.js
@@ -0,0 +1,19 @@
+const accountId = 4424;
+let accountEmail = "roshankrsu@gmal.com";
+var accountPssword = "12345";
+accountCity = "Ranchi";
+let accountState;
+// accountId = 2;
+accountEmail = "ros@gmail.com"
+accountPssword = "2312132"
+accountCity= "samastipur" 
+
+// console.log(accountId);
+
+/*
+prefer not to use var because of issue in block scope and functional scope
+*/
+
+
+
+console.table([accountId,accountEmail,accountPssword,accountCity,accountState])
