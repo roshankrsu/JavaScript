b96f2279cb8350fcb58808978f50180f1d61cca7
Roshan <roshankrsu@gmail.com>
2025-07-08 06:11:28 +0000

    Conversion operation

---
+++ basics/conversionOperation.js
@@ -0,0 +1,42 @@
+let score = true;
+
+// console.log(typeof score);
+
+
+let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+let isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+let someNumber = 43
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+
+// console.log(typeof stringNumber);
+
+
+//***********Operation*************
+let value = 3;
+let negValue = -value
+// console.log(negValue);
+
+let str1 = "hello"
+let str2 = " roshan"
+let str3 = str1+str2
+// console.log(str3) 
+// console.log(+true)
+// console.log(+"")
+let num1, num2,num3
+ 
+num1 = num2 = num3 = 3+2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
