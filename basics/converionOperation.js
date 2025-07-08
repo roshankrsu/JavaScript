8c7d8317ab8871a14d77c14b7e5232e1cd177870
Roshan <roshankrsu@gmail.com>
2025-07-08 05:26:26 +0000

    conversion of data types

---
+++ basics/converionOperation.js
@@ -0,0 +1,22 @@
+let score = true;
+
+// console.log(typeof score);
+
+
+let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+let isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+let someNumber = 43
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+
+console.log(typeof stringNumber);
