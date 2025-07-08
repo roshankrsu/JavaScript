d31fdf76026ac79f1108e6b4fbfeca4b327356ac
Roshan <roshankrsu@gmail.com>
2025-07-08 06:47:12 +0000

    data types summery

---
+++ basics/dataTypes_summery.js
@@ -0,0 +1,36 @@
+// #primitive datatype
+
+// 7 types:String ,Number , Boolean, null,undefined,Symbol,BigInt
+
+const score = 100
+const scoreValue = 100.3
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id=== anotherId);
+
+// const bigNumber = 4342342342343243354545n
+
+
+
+
+
+// #reference datatype(non-primitive)
+
+//array,Objects,functions
+
+const heros = ["shaktiman","spiderman","ironman"]
+let myObj={
+    name:"roshan",
+    age: 20,
+}
+
+const myFunction =function(){
+    console.log("Hello world");
+}
+
+console.log(typeof bigNumber);
