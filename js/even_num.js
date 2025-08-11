72d83ae2431e2af53f1fcba6a1b66c5f869b6be8
roshankrsu <roshankrsu@email.com>
2025-08-11 11:53:18 +0530

    check even and odd number in array

---
+++ js/even_num.js
@@ -0,0 +1,9 @@
+num = [5,51,43,64]
+
+for(i=0;i<num.length;i++){
+    if(num[i]%2==0){
+        console.log(` ${num[i]} is even number`)
+    }else{
+        console.log(`${num[i]} is odd number`)
+    }
+}
